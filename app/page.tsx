8d46e944e9cf249724d556cf7d32d6bf7dd932db
amro22397 <amroalmutasim22@gmail.com>
2024-10-08 12:04:11 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/Container"
+import HomeBanner from "@/components/HomeBanner"
+
+const page = () => {
+  return (
+    <div className='p-8'>
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default page
