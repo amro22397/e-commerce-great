134e098a271ec197ac170f09719b20ca6445e98f
amro22397 <amroalmutasim22@gmail.com>
2024-10-16 05:39:35 +0400

    add review and product model

---
+++ models/Review.ts
@@ -0,0 +1,33 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const ReviewSchema = new Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId, ref: "id"
+    },
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref: "userId"
+    },
+
+    productId: {
+        type: mongoose.Schema.Types.ObjectId, ref: "productId"
+    },
+
+    rating: {
+        type: Number,
+    },
+
+    comment: {
+        type: String,
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    
+    
+}, { timestamps: true }
+)
+
+export const Review = models?.Review || model('Review', ReviewSchema)
