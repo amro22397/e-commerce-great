b19e5bbc544a91b436d861718d7ea968d77d3328
amro22397 <amroalmutasim22@gmail.com>
2024-10-16 06:41:07 +0400

    add checbox and textarea

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,12 +1,98 @@
-'use client'
+"use client";
 
-import React from 'react'
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+import firebaseApp from "@/libs/firebase";
+import { useCallback, useEffect, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Input from "@/components/inputs/Input";
+import TextArea from "@/components/inputs/TextArea";
+import CustomCheckBox from "@/components/inputs/CustomCheckBox";
+
+export type ImageType = {
+    color: string;
+    colorCode: string;
+    image: File | null;
+  };
+  
+  export type UploadedImageType = {
+    color: string;
+    colorCode: string;
+    image: string;
+  };
 
 const AddProductForm = () => {
+
+    const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState<ImageType[] | null>();
+  const [isProductCreated, setIsProductCreated] = useState(false);
+
+  const {register, handleSubmit, setValue, watch, reset, formState: {errors}} = useForm<FieldValues>({
+    defaultValues: {
+        name: "",
+      description: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      price: "",
+    }
+  })
+
   return (
-    <div>
+    <>
+      <Heading title="Add a Product" center />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        type="number"
+        required
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
       
-    </div>
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This product is in stock"
+      />
+    </>
   )
 }
 
