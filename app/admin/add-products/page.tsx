3a132e45c020020e066f2cad4ddff9f4f8824be0
amro22397 <amroalmutasim22@gmail.com>
2024-10-16 06:14:51 +0400

    make null data

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Container from "@/components/Container";
+import FormWrap from "@/components/FormWrap";
+import AddProductForm from "./AddProductForm";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/components/NullData";
 
-const page = () => {
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser._doc.role !== "ADMIN") {
+    return (
+        <>
+        <pre className="hidden">{JSON.stringify(currentUser, null, 2)}</pre>
+        <NullData title="You are not allowed to access this page 21" />
+        </>
+    );
+  }
+
   return (
-    <div>
-      
+    <div className="p-8">
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default AddProducts;
